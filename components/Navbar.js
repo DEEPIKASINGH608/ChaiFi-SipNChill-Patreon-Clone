81d8e56bfd5ab05392aefd2e865bfcaa9414b833
DEEPIKASINGH608 <ds0648775@gmail.com>
2026-05-10 11:20:11 +0530

    Fixed some errors

--- components/Navbar.js
+++ components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
                         {/* Dropdown Menu */}
                         {isOpen && (
                             <>
-    
+
                                 <div
                                     className="fixed inset-0 z-0"
                                     onClick={() => setIsOpen(false)}
